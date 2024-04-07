7ce6dfad9e10693aad64f11b2a8e7c4f0a6daa38
Wild-Design <brenneke_ruger@hotmail.com>
2024-04-06 21:04:16 -0300

    esPrimo completed

--- src/randomFunctions.js
+++ src/randomFunctions.js
@@ -2,9 +2,23 @@ const sumaDosNumeros = (num1, num2) => {
   if (typeof num1 !== 'number' || typeof num2 !== 'number')
     throw new Error('Ambos argumentos deben ser numeros');
 
+  if (num1 < 0 || num2 < 0)
+    throw new Error('Ambos argumentos deben ser positivos');
   return num1 + num2;
+};
+
+const esPrimo = (num) => {
+  if (num <= 0) throw new Error('El numero debe ser superior a 0');
+  if (typeof num !== 'number')
+    throw new Error('El argumento debe ser un numero');
+  if (num === 1) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
 };
 
 module.exports = {
   sumaDosNumeros,
+  esPrimo,
 };
--- tests/randomFunctions.spec.js
+++ tests/randomFunctions.spec.js
@@ -1,25 +1,48 @@
 const randomFunctions = require('../src/randomFunctions');
 
-const { sumaDosNumeros } = randomFunctions;
+const { sumaDosNumeros, esPrimo } = randomFunctions;
 
 describe('Funcion sumaDosNumeros', () => {
   test('Debe retornar la suma de dos numeros positivos correctamente', () => {
     expect(sumaDosNumeros(5, 10)).toBe(15);
   });
   test('Debería arrojar un error si alguno de los argumentos no es un numero', () => {
-    expect(sumaDosNumeros('2', 3)).toThrow(
-      new Error('Ambos argumentos deben ser numeros')
+    expect(() => sumaDosNumeros('2', 3)).toThrow(
+      'Ambos argumentos deben ser numeros'
     );
-    expect(sumaDosNumeros(2, '3')).toThrow(
-      new Error('Ambos argumentos deben ser numeros')
+    expect(() => sumaDosNumeros(2, '3')).toThrow(
+      'Ambos argumentos deben ser numeros'
     );
   });
   test('Debería arrojar un error si alguno de los argumentos es negativo', () => {
-    expect(sumaDosNumeros(-2, 9)).toThrow(
+    expect(() => sumaDosNumeros(-2, 9)).toThrow(
       'Ambos argumentos deben ser positivos'
     );
-    expect(sumaDosNumeros(2, -9)).toThrow(
+    expect(() => sumaDosNumeros(2, -9)).toThrow(
       'Ambos argumentos deben ser positivos'
     );
+  });
+});
+
+describe('Función esPrimo', () => {
+  test('Debe retornar true si el numero es primo', () => {
+    const primos = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+    for (const primo of primos) {
+      expect(esPrimo(primo)).toBe(true);
+    }
+  });
+  test('Debería arrojar un error si el numero recivido es 0 o menor', () => {
+    expect(() => esPrimo(0)).toThrow('El numero debe ser superior a 0');
+  });
+  test('Debería arrojar un error si el argumento no es un numero', () => {
+    expect(() => esPrimo('Esto no es un numero')).toThrow(
+      'El argumento debe ser un numero'
+    );
+  });
+  test('Debería retornar false si el numero no es primo', () => {
+    const noPrimos = [1, 4, 6, 8, 9, 10, 12, 14, 15, 16];
+    for (const noPrimo of noPrimos) {
+      expect(esPrimo(noPrimo)).toBe(false);
+    }
   });
 });
